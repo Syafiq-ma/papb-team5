94f80ca785e0370564da97dd5efa6fffd110f7b5
Daffa Bilnadzary <daffa.bil.nadzary@365.ugm.ac.id>
2021-10-08 23:38:15 +0700

    create layout for detailTugas

---
+++ app/src/main/java/com/example/papb_team5/detailTugas.kt
@@ -0,0 +1,11 @@
+package com.example.papb_team5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detailTugas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_tugas)
+    }
+}
