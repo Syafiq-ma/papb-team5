b05e5334f3aed1ed510f5180004f6e319e05095b
Daffa Bilnadzary <daffa.bil.nadzary@365.ugm.ac.id>
2021-10-12 00:25:13 +0700

    implementing the navbar to Home Page fragment

--- app/src/main/java/com/example/papb_team5/MainActivity.kt
+++ app/src/main/java/com/example/papb_team5/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.papb_team5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import fragmentFirst
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainFragment = fragmentFirst()
+        setCurrentFragment(mainFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(mainFragment)
+                //.id.calendar->setCurrentFragment()
+                //R.id.profile->setCurrentFragment()
+            }
+            true
+        }
     }
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
 }
