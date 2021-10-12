a5076310be82e14c207f3e34cd8e42de82f0cdfb
Daffa Bilnadzary <daffa.bil.nadzary@365.ugm.ac.id>
2021-10-12 20:15:46 +0700

    add navigation from fragment_home to detailTugas

--- app/src/main/java/com/example/papb_team5/detailTugas.kt
+++ app/src/main/java/com/example/papb_team5/detailTugas.kt
@@ -1,11 +1,19 @@
 package com.example.papb_team5
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail_tugas.*
 
 class detailTugas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_tugas)
+
+        btn_back.setOnClickListener{
+
+            var intent = Intent(this@detailTugas, fragment_home::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/papb_team5/fragment_home.kt
+++ app/src/main/java/com/example/papb_team5/fragment_home.kt
@@ -1,12 +1,25 @@
 package com.example.papb_team5
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat.finishAffinity
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.coroutines.Dispatchers.Main
 
 class fragment_home : Fragment(R.layout.fragment_home) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        view4.setOnClickListener {
+            activity?.let {
+                val intent = Intent(it, detailTugas::class.java)
+                it.startActivity(intent)
+            }
+        }
+    }
 }
 
