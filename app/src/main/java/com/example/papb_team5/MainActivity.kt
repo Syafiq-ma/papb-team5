51f59727c8cdfffe1814ad89611c2829b2c9f265
Daffa Bilnadzary <daffa.bil.nadzary@365.ugm.ac.id>
2021-11-23 14:15:50 +0700

    trying to connect the data with the app

--- app/src/main/java/com/example/papb_team5/MainActivity.kt
+++ app/src/main/java/com/example/papb_team5/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.papb_team5
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.papb_team5.application.TasksApplication
+import com.example.papb_team5.view_model.TaskViewModel
+import com.example.papb_team5.view_model.TaskViewModelFactory
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/example/papb_team5/fragment_home.kt
+++ app/src/main/java/com/example/papb_team5/fragment_home.kt
@@ -1,22 +1,45 @@
 package com.example.papb_team5
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.papb_team5.adapter.taskItemAdapter
 import com.example.papb_team5.adapter.toDoItemAdapter
+import com.example.papb_team5.application.TasksApplication
 import com.example.papb_team5.data.datasource
+import com.example.papb_team5.view_model.TaskViewModel
+import com.example.papb_team5.view_model.TaskViewModelFactory
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class fragment_home : Fragment(R.layout.fragment_home) {
+
+    private val newTaskActivityRequestCode = 1
+    private val taskViewModel: TaskViewModel by viewModels {
+        TaskViewModelFactory((application as TasksApplication).repository)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val myDataset = datasource().loadAffirmations()
+        /*
+        //val myDataset = datasource().loadAffirmations()
 
         val recyclerView = getView()?.findViewById<RecyclerView>(R.id.todo_recycler)
         recyclerView?.adapter = toDoItemAdapter(this, myDataset)
 
-        recyclerView?.setHasFixedSize(true)
+        recyclerView?.setHasFixedSize(true)*/
+        val recyclerView = getView()?.findViewById<RecyclerView>(R.id.todo_recycler)
+        recyclerView?.adapter = taskItemAdapter()
+
+        val fab = getView()?.findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener{
+            val intent = Intent(this@fragment_home, NewTaskActivity::class.java)
+            startActivityForResult(intent, newTaskActivityRequestCode)
+        }
     }
 }
 
